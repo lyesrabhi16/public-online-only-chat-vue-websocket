64f600ca750f4f0f6c5f9a2d73e1bfd080f7e2b9
lyesrabhi16 <lyes.rabhi.020@gmail.com>
2024-10-18 14:06:37 +0100

    update

--- src/components/client.vue
+++ src/components/client.vue
@@ -1,53 +1,51 @@
 <script setup lang="ts">
-    import { ref, onMounted } from 'vue';
-    import { io } from 'socket.io-client';
-    import { Socket } from "socket.io-client";
-    const messages = ref<string[]>([]);
-    let socket : Socket; 
-    const addMessage = (message: string) => {
-        console.log('Adding message:', message);
-        
-        messages.value = [...messages.value, message];
-    };
+import { ref, onMounted } from "vue";
+import { io } from "socket.io-client";
+import { Socket } from "socket.io-client";
+const messages = ref<string[]>([]);
+let socket: Socket;
+const addMessage = (message: string) => {
+  console.log("Adding message:", message);
 
-    const handleSubmit = (event: Event) => {
-        event.preventDefault();
-        const input = document.getElementById('send_message') as HTMLInputElement;
-        if (socket){
-            socket.emit('message', input.value);
-            input.value = '';
-        }
-    };
+  messages.value = [...messages.value, message];
+};
 
-    onMounted(()=> {
-        socket = io('ws://localhost:3000');
-        socket.on('connect', () => {
-            console.log('Connected to server');
-        });
-        socket.on('message', (message: string) => {
-            addMessage(message);
-        });
-        socket.on('disconnect', () => {
-            console.log('Disconnected from server');
-        });
-    });
+const handleSubmit = (event: Event) => {
+  event.preventDefault();
+  const input = document.getElementById("send_message") as HTMLInputElement;
+  if (socket) {
+    socket.emit("message", input.value);
+    input.value = "";
+  }
+};
 
+onMounted(() => {
+  socket = io("ws://localhost:3000");
+  socket.on("connect", () => {
+    console.log("Connected to server");
+  });
+  socket.on("message", (message: string) => {
+    addMessage(message);
+  });
+  socket.on("disconnect", () => {
+    console.log("Disconnected from server");
+  });
+});
 </script>
 
 <template>
-    
-    <ul>
-        <li v-for="message in messages" :key="message">{{ message }}</li>
-    </ul>
-    <form @submit="handleSubmit">
-        <input id="send_message" type="text" />
-        <button type="submit">Add</button>
-    </form>
+  <ul>
+    <li v-for="message in messages" :key="message">{{ message }}</li>
+  </ul>
+  <form @submit="handleSubmit">
+    <input id="send_message" type="text" />
+    <button type="submit">Add</button>
+  </form>
 </template>
 
 <style scoped>
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 </style>
